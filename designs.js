46585fd0032016db4021c17e0c69a43526de64da
Bruno Gomes <brunofreiregomes@gmail.com>
2018-04-19 23:44:51 -0300

    Development of the project

--- designs.js
+++ designs.js
@@ -2,9 +2,31 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+const pixelCanvas = $('#pixelCanvas');
+
+/**
+ This function is responsable for creating the grid with the
+ height and width configuration sended by the user.
+*/
 
 function makeGrid() {
 
-// Your code goes here!
+	const gridHeight = $('#inputHeight').val();
+	const gridWidth = $('#inputWidth').val();
+	pixelCanvas.text('');
 
+	for(let i = 1; i <= gridHeight; i++){
+		const row = $('<tr></tr>').appendTo(pixelCanvas);
+		for(let x = 1; x <= gridWidth; x ++){
+			$('<td></td>').appendTo(row);
+		}
+	}
+	
+	event.preventDefault();
 }
+
+$(pixelCanvas).on('click','td', function(){
+	const colorPicker = $('#colorPicker');
+	let selectedColor = colorPicker.val();
+	$(this).css('background-color', selectedColor);
+});
